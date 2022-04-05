57f7e7e2b9abe8133ba7df949db84551f5883987
loganator956 <werktxgnpyerpvgmuokc@gmail.com>
2022-04-05 18:02:37 +0100

    Read files into arrays

--- Program.cs
+++ Program.cs
@@ -1,2 +1,31 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace CMP1124M_Assignment
+{
+    public class Program
+    {
+        private const int NumberOfShares = 3;
+        public static void Main(string[] args)
+        {
+            int[,] shareValues256 = new int[NumberOfShares, 256];
+            for (int i = 0; i < NumberOfShares; i++)
+            {
+                string fileName = $"Share_{i + 1}_256.txt";
+                if (!File.Exists(fileName))
+                {
+                    Console.WriteLine($"\nERROR: CANNOT FIND FILE: {fileName}\nExiting");
+                    return;
+                }
+                string[] lines = File.ReadAllLines(fileName);
+                for (int line = 0; line < lines.Length; line++)
+                {
+                    shareValues256[i, line] = int.Parse(lines[line]);
+                }
+            }
+
+            // TODO: Sort into ascending and descending order
+
+            // TODO: Display every 10th value of the SELECTED arrays
+        }
+    }
+}

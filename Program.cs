aff07ac7883af4cb1878d2127f6c9c6803e47ffa
loganator956 <65763483+loganator956@users.noreply.github.com>
2022-04-09 13:22:22 +0100

    converted arrays to jagged arrays

--- Program.cs
+++ Program.cs
@@ -7,7 +7,12 @@ namespace CMP1124M_Assignment
         private const int NumberOfShares = 3;
         public static void Main(string[] args)
         {
-            int[,] shareValues256 = new int[NumberOfShares, 256];
+            int[][] shareValues256 = new int[NumberOfShares][];
+            for (int i = 0; i < shareValues256.Length; i++)
+            {
+                // initializing the jagged array
+                shareValues256[i] = new int[256];
+            }
             for (int i = 0; i < NumberOfShares; i++)
             {
                 string fileName = $"Share_{i + 1}_256.txt";
@@ -19,11 +24,17 @@ namespace CMP1124M_Assignment
                 string[] lines = File.ReadAllLines(fileName);
                 for (int line = 0; line < lines.Length; line++)
                 {
-                    shareValues256[i, line] = int.Parse(lines[line]);
+                    shareValues256[i][line] = int.Parse(lines[line]);
                 }
             }
 
-            // TODO: Sort into ascending and descending order
+            // TODO: Select Arrays
+            // TODO: Allow for selecting multiple arrays
+            MessagePrompt message = new MessagePrompt($"Select which array to use (1-3)");
+            int selectedArrayIndex = message.ShowMessageNumericalResponse(1,3) - 1;
+
+            // TODO: Sort into ascending and descending order 
+            
 
             // TODO: Display every 10th value of the SELECTED arrays
         }

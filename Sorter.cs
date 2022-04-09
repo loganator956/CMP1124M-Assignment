d2bbf8e27c1f9c6222fe08a0b8e45a4d7bbce0c6
loganator956 <65763483+loganator956@users.noreply.github.com>
2022-04-09 13:21:54 +0100

    added bubble sort

---
+++ Sorter.cs
@@ -0,0 +1,30 @@
+using System;
+
+namespace CMP1124M_Assignment
+{
+    public static class Sorter
+    {
+        public static int[] BubbleSort(int[] input)
+        {
+            int[] currentOrder = input;
+            for (int j = 0; j < input.Length - 1; j++)
+            {
+                for (int i = 0; i < input.Length - 1; i++)
+                {
+                    if (currentOrder[i] < currentOrder[i + 1])
+                    {
+                        // sorting in ascending order by default so this is already in correct order
+                    }
+                    else
+                    {
+                        // need to swap the two values
+                        int t = currentOrder[i + 1];
+                        currentOrder[i + 1] = currentOrder[i];
+                        currentOrder[i] = t;
+                    }
+                }
+            }
+            return currentOrder;
+        }
+    }
+}

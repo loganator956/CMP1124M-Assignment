becf9b4fefe320107632b22d8cde41bf44d5e562
loganator956 <65763483+loganator956@users.noreply.github.com>
2022-04-23 12:20:42 +0100

    implemented merge sort

--- Sorter.cs
+++ Sorter.cs
@@ -11,11 +11,7 @@ namespace CMP1124M_Assignment
             {
                 for (int i = 0; i < input.Length - 1; i++)
                 {
-                    if (currentOrder[i] < currentOrder[i + 1])
-                    {
-                        // sorting in ascending order by default so this is already in correct order
-                    }
-                    else
+                    if (!(currentOrder[i] < currentOrder[i + 1]))
                     {
                         // need to swap the two values
                         int t = currentOrder[i + 1];
@@ -25,6 +21,81 @@ namespace CMP1124M_Assignment
                 }
             }
             return currentOrder;
+        }
+
+        public static int[] BeginMergeSort(int[] input)
+        {
+            int[] result = MergeSort(input.ToList()).ToArray();
+            return new int[] { };
+        }
+
+        private static List<int> MergeSort(List<int> unsorted)
+        {
+            if (unsorted.Count <= 1)
+            {
+                // can not split the list any further so return.
+                return unsorted;
+            }
+
+            // set up the new lists for the sub-arrays
+            List<int> left = new List<int>();
+            List<int> right = new List<int>();
+
+            // calculate the midpoint to split the list into
+            int middle = unsorted.Count / 2;
+
+            // split the list into left/right sub-lists
+            for (int i = 0; i < middle; i++)
+            {
+                left.Add(unsorted[i]);
+            }
+            for (int i = middle; i < unsorted.Count; i++)
+            {
+                right.Add(unsorted[i]);
+            }
+
+            // begin/continue the recursive function until reach the end
+            left = MergeSort(left);
+            right = MergeSort(right);
+            return Merge(left, right);
+        }
+
+        private static List<int> Merge(List<int> left, List<int> right)
+        {
+            List<int> result = new List<int>();
+
+            // while there are still elements left to be processed in left OR right arrays
+            while (left.Count > 0 || right.Count > 0)
+            {
+                // if there are still comparisons to be made
+                if (left.Count > 0 && right.Count > 0)
+                {
+                    // compare the first values of the lists
+                    if (left.First() <= right.First())
+                    {
+                        result.Add(left.First());
+                        left.RemoveAt(0);
+                    }
+                    else
+                    {
+                        result.Add(right.First());
+                        right.RemoveAt(0);
+                    }
+                }
+                // Some situations lead to having one of the sides having one more/less element than the other
+                // so check and deal with the remaining element
+                else if (left.Count > 0)
+                {
+                    result.Add(left.First());
+                    left.RemoveAt(0);
+                }
+                else if (right.Count > 0)
+                {
+                    result.Add(right.First());
+                    right.RemoveAt(0);
+                }
+            }
+            return result;
         }
 
         public static int[] ReverseOrder(int[] input)
